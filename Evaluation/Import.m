9b379735066b5bcee870577ce27c8e891216b052
MochTor <tieghi.marco@gmail.com>
2018-02-02 02:25:48 +0100

    Refined importing process in the import module, corrected TREC runs format for better importing

--- Evaluation/Import.m
+++ Evaluation/Import.m
@@ -1,18 +1,14 @@
-addpath('/Users/Marco/Documents/GitHub/ProjectIR/core/io'); %Library for IO functions
+addpath('/Users/Marco/Documents/GitHub/IR_Project/Evaluation/MATTERS_Core/io'); %Library for IO functions
 
 %%%%%%%%%%% Importing pool and run %%%%%%%%%%
-poolFileName = '/Users/Marco/Documents/GitHub/ProjectIR/results/qrels.trec7.txt'; %pool file path
+poolFileName = '/Users/Marco/Documents/GitHub/IR_Project/qrels.trec7.txt'; %pool file path
 relevanceGrades = 0:1;  %Pool relevance grades vector
 relevanceDegree = {'NotRelevant', 'Relevant'};  %Pool relevance degree vector
-poolId = 'Pool_TREC';
-fprintf('>>>>>>>> Importing pool <<<<<<<<<<');
+poolId = 'TREC_Pool';
+fprintf('>>>>>>>> Importing pool <<<<<<<<<< \n');
 [pool, report] = importPoolFromFileTRECFormat('FileName', poolFileName, 'Identifier', poolId, 'RelevanceGrades', relevanceGrades, 'RelevanceDegree', relevanceDegree); %importing pool
 
-runFileName = '/Users/Marco/Documents/GitHub/ProjectIR/results/BB2c1.0_0.res';  %run file path
-documentOrdering = 'TrecEvalLexDesc';
-fprintf('>>>>>>>> Importing run <<<<<<<<<<');
-%[run, report] = importRunFromFileTRECFormat('FileName', runFileName, 'DocumentOrdering', documentOrdering, 'Delimiter', 'space');
-
-runsDirectoryFilePath = '/Users/Marco/Documents/GitHub/ProjectIR/results/';
-[runSet, report] = importRunsFromDirectoryTRECFormat('Path', runsDirectoryFilePath, 'Identifier', poolId, 'Delimiter', 'tab');
-fprintf('>>>>>>>> End <<<<<<<<<<');
+fprintf('>>>>>>>> Importing runs <<<<<<<<<< \n');
+runsDirectoryFilePath = '/Users/Marco/Documents/GitHub/IR_Project/Runs';
+[runSet, report] = importRunsFromDirectoryTRECFormat('Path', runsDirectoryFilePath, 'Identifier', 'basicRuns', 'Delimiter', 'space');
+fprintf('>>>>>>>> End of importing process <<<<<<<<<<');
