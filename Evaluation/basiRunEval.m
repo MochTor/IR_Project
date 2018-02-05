c8dd71257a0d8a245b1d68635fc523fda0886166
MochTor <tieghi.marco@gmail.com>
2018-02-05 20:29:56 +0100

    Better code formatting

--- Evaluation/basiRunEval.m
+++ Evaluation/basiRunEval.m
@@ -9,14 +9,4 @@ load('/Users/Marco/Documents/GitHub/IR_Project/Evaluation/basicRunSetData.mat');
 
 %%%%%%%%%%% Calculating AP %%%%%%%%%%
 fprintf('>>>>>>>>>> Measuring Average Precision <<<<<<<<<<<\n');
-%measuredRunSet = averagePrecision(pool, basicRunSet(:,1));
-%[basicRunSet, basicRunReport] = importRunsFromDirectoryTRECFormat('Path', runsDirectoryFilePath, 'Identifier', 'basicRuns', 'Delimiter', 'space');
-
-
-%for i = 1 : 10
-   %currentRun = basicRunSet(:,1);
-   %currentRun.Properties.UserData.identifier = strcat('basicRun', num2str(i));
-   %basicRunSet(:,i) = currentRun;
-   %currentRun = importRunFromFileTRECFormat()    
-%end
 [assessedRunSet, poolStats, runSetStats, inputParams] = assess(pool, basicRunSet);
